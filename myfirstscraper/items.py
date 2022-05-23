ee0a6362e7f7f1c06ec6e1ffb63f7fa81d31a4da
AdamPrzyboro <a.m.przyboroski@gmail.com>
2022-05-23 09:29:44 +0200

    [ScrapyInitProject] init commit, simple spider

---
+++ myfirstscraper/items.py
@@ -0,0 +1,12 @@
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ProductPhoneItem(scrapy.Item):
+    name = scrapy.Field()
+    price = scrapy.Field()
+    vendor = scrapy.Field()
+
+
